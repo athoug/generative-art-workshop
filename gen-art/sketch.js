bc13500d83fc781889c8b16ffad2f7bf772897ae
Athoug <athoug.s@gmail.com>
2021-03-13 14:47:32 +0300

    basic canvas api

--- gen-art/sketch.js
+++ gen-art/sketch.js
@@ -6,8 +6,17 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#0a00b6';
     context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width/2, height/2, 300, 0, Math.PI * 2, false);
+    context.fillStyle = '#9d46ff';
+    context.fill();
+    context.strokeStyle = '#6200ea';
+    context.lineWidth = 20;
+    context.stroke();
+
   };
 };
 
