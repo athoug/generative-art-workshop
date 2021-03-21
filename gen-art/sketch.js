06c7e12050b6b8434731cfc5a2fe04111a75acb2
Athoug <athoug.s@gmail.com>
2021-03-21 09:21:35 +0300

    canvas application setup

--- gen-art/sketch.js
+++ gen-art/sketch.js
@@ -1,20 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: 'A4',
+  units: 'cm',
+  orientation: 'landscape',
+  pixelsPerInch: 300,
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
+    console.log(width, height);
     context.fillStyle = '#0a00b6';
     context.fillRect(0, 0, width, height);
 
     context.beginPath();
-    context.arc(width/2, height/2, 300, 0, Math.PI * 2, false);
+    context.arc(width/2, height/2, width * 0.2, 0, Math.PI * 2, false);
     context.fillStyle = '#9d46ff';
     context.fill();
     context.strokeStyle = '#6200ea';
-    context.lineWidth = 20;
+    context.lineWidth = width * 0.05;
     context.stroke();
 
   };
