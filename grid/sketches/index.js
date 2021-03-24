6d8d76deed0ec0572bc8bc855c01bed6f2ca1ace
Athoug <athoug.s@gmail.com>
2021-03-24 12:12:29 +0300

    practice grid

---
+++ grid/sketches/index.js
@@ -0,0 +1,46 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+
+  const createGrid = () => {
+    const points = [];
+    const grid = 5;
+
+    for (let x = 0; x < grid; x++) {
+      for (let y = 0; y < grid; y++) {
+        const u = grid <= 1 ? 0.5 : x / (grid - 1);
+        const v = grid <= 1 ? 0.5 : y / (grid - 1);
+        
+        points.push([u,v]);
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid();
+  const margin = 400;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = '#212121';
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(([u,v]) => {
+      const x = lerp(margin, (width - margin), u);
+      const y = lerp(margin, (height - margin), v);
+
+      context.beginPath();
+      context.arc(x, y, 50, 0, Math.PI * 2, false);
+      context.strokeStyle = '#F5F5F6';
+      context.lineWidth = 20;
+      context.stroke();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
