bf5e167e51c97c43774bc719c037f0d9ecea7c8c
Athoug <athoug.s@gmail.com>
2021-04-10 11:19:01 +0300

    spades

--- cards/cards.js
+++ cards/cards.js
@@ -7,23 +7,33 @@ const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
-const palette = random.pick(palettes);
-const bg = palette.shift();
-const margin = 350;
-const counter = 20;
+const sketch = ({ width, height }) => {
+  const characters = '♠♣♥♦'.split('');
+  const palette = random.pick(palettes);
+  const bg = palette.shift();
+  const counter = 25;
+  const margin = width * 0.15;
 
-const sketch = () => {
   const createGrid = () => {
     const points = [];
+    const frequency = random.range(0.75, 1.25);
+
     for (let x = 0; x < counter; x++) {
       for (let y = 0; y < counter; y++) {
-        const u = counter <=1 ? 0.5 : x / (counter - 1);
-        const v = counter <=1 ? 0.5 : y / (counter - 1);
-        const radius = random.value() * 0.4;
+        const u = counter < 0 ? 0.5 : x / (counter - 1);
+        const v = counter < 0 ? 0.5 : y / (counter - 1);
+
+        const n = random.noise2D(u * frequency, v * frequency);
+        const size = n * 0.5 + 0.5;
+        const baseSize = width * 0.05;
+        const sizeOffset = width * 0.07;
+
         points.push({
-          position: [u, v], 
           color: random.pick(palette),
-          radius,
+          size: Math.abs(baseSize * size + random.gaussian() * sizeOffset),
+          character: random.pick(characters),
+          rotation: n * Math.PI * 0.5,
+          position: [u, v], 
         });
       }
     }
@@ -32,7 +42,7 @@ const sketch = () => {
   }
 
   const points = createGrid().filter(() => Math.random() > 0.75);
-
+  
   return ({ context, width, height }) => {
     context.fillStyle = bg;
     context.fillRect(0, 0, width, height);
@@ -40,15 +50,26 @@ const sketch = () => {
     points.forEach(data => {
       const {
         position, 
-        radius, 
+        size, 
         color,
+        character,
+        rotation,
       } = data;
       const [u, v] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
+      
       context.fillStyle = color;
-      context.arc(x, y, 5, 0, Math.PI * 2);
-      context.fill();
+      context.textAlign = 'center';
+      context.textBaseline = 'middle';
+      context.font = `${size}px Helvetica`;
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText(character, 0, 0);
+      context.restore();
     });
   };
 };
