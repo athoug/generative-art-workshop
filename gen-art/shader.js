7f1f8e1928907b603952f496b3ccd9fb5098860f
Athoug <athoug.s@gmail.com>
2021-04-23 10:57:50 +0300

    Shape & Form

--- gen-art/shader.js
+++ gen-art/shader.js
@@ -4,6 +4,7 @@ const glsl = require('glslify');
 
 // Setup our sketch
 const settings = {
+  // dimensions: [1024, 1024],
   context: 'webgl',
   animate: true
 };
@@ -13,12 +14,19 @@ const frag = glsl(/* glsl */`
   precision highp float;
 
   uniform float time;
+  uniform float aspect;
   varying vec2 vUv;
 
   void main () {
-    // vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    // gl_FragColor = vec4(color, 1.0);
-    gl_FragColor = vec4(vec3(vUv.x), 1.0);
+    vec3 colorA = vec3(1.0, 0.0, 0.0);
+    vec3 colorB = vec3(0.0, 1.0, 0.0);
+
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+    float dist = length(center);
+
+    vec3 color = mix(colorA, colorB, vUv.x);
+    gl_FragColor = vec4(color, dist > 0.25 ? 0.0 : 1.0);
   }
 `);
 
@@ -33,7 +41,8 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time
+      time: ({ time }) => time,
+      aspect: ({ width, height}) => width / height,
     }
   });
 };
